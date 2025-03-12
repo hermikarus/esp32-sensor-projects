7b1826fd0fb915d14e7ebe67a847a6b130cf1872
Özgür Egemen <119122327+hermikarus@users.noreply.github.com>
2025-03-12 13:31:44 +0300

    english version main.cpp

--- arduino-framework/pir_sensor_project/pir_sensor_project/src/main.cpp
+++ arduino-framework/pir_sensor_project/pir_sensor_project/src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 
-#define PIR_PIN 15  // PIR sensörünün bağlandığı GPIO pini
+#define PIR_PIN 15  // GPIO pin where the PIR sensor is connected
 
 void setup() {
     Serial.begin(115200);
     pinMode(PIR_PIN, INPUT);
-    Serial.println("PIR Sensor Testi Basliyor...");
+    Serial.println("PIR Sensor Test Starting...");
 }
 
 void loop() {
     int motionState = digitalRead(PIR_PIN);
     
     if (motionState == HIGH) {
-        Serial.println("Hareket var!");
+        Serial.println("Motion detected!");
     } else {
-        Serial.println("Hareket yok...");
+        Serial.println("No motion detected...");
     }
 
-    delay(1000); // Sensörün çıkış durumunu kontrol etmek için bekleme
+    delay(1000); // Wait 1 second before checking the sensor output again
 }
